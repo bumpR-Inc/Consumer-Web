c3fe888fa3b6445cef3548312544200dd436c5b7
Justin Quan <justquan@berkeley.edu>
2021-02-22 06:24:41 -0800

    some menupage responsiveness changes

--- src/components/OrderingUI/VenmoBtn.tsx
+++ src/components/OrderingUI/VenmoBtn.tsx
@@ -2,13 +2,33 @@ import React from 'react'
 import venmoImg from "../../assets/img/ui/venmo.png";
 import { Link } from "@reach/router";
 
+import { makeStyles } from "@material-ui/core/styles";
+import { theme } from "../Theme";
 
+
+const useStyles = makeStyles({
+  venmoBtn: {
+    width: "25vw",
+    maxWidth: "215px",
+    margin: ".5rem",
+    [theme.breakpoints.down("md")]: {
+      width: "46vw",
+      maxWidth: "165px",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "46vw",
+      maxWidth: "165px",
+    },
+  },
+});
 
 export default function VenmoBtn(props : any) {
+    var classes = useStyles();
+
     return (
       <div>
         <a href={props.paymentLink}>
-          <img className="venmo-btn" src={venmoImg}></img>
+          <img className={classes.venmoBtn} src={venmoImg}></img>
         </a>
       </div>
     );
