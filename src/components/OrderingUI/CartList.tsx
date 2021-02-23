5292faf6d219deb53ff1464ee125e058dd3a31c9
Justin Quan <justquan@berkeley.edu>
2021-02-23 14:28:03 -0800

    removed duplicates after refreshing in cart

--- src/components/OrderingUI/CartList.tsx
+++ src/components/OrderingUI/CartList.tsx
@@ -12,15 +12,44 @@ const useStyles = makeStyles({
 
 
 export default function CartList(props: any): Array<JSX.Element> {
-  const classes = useStyles(); 
+  const classes = useStyles();
   const { meals, toggleFavAction, orders, store } = props;
   const { state, dispatch } = store;
 
-  const uniqueMealsArray = Array.from(new Set<IMeal>(orders)); //Important: converts array to set, removing duplicates and then convert back to array
+  const mealsPks = orders.map((meal: IMeal) => meal.pk);
+  const uniqueMealsPks = Array.from(new Set<any>(mealsPks));
+  const uniqueMealsArray = state.meals.filter((curr: IMeal) =>
+    uniqueMealsPks.includes(curr.pk)
+  ); //Important: converts array to set, removing duplicates and then convert back to array
+
+  // const uniqueMealsArray = uniqueMealsPks.map(())
+
+  // var uniqueMealsArray: Array<IMeal> = [];
+  // var seenPks: Array<Number> = [];
+  // var ordersIndex = 0
+  // while (ordersIndex < orders.length) {
+  //   var currMeal: IMeal = orders[ordersIndex];
+  //   // if (!seenPks.includes(currMeal.pk)) {
+  //     uniqueMealsArray.concat(currMeal);
+  //     seenPks.concat(ordersIndex);
+  //   // }
+  //   ordersIndex = ordersIndex + 1;
+  // }
+
+  // const uniqueMealsArray = Array.from(new Set<IMeal>(orders)); //Important: converts array to set, removing duplicates and then convert back to array
+  // const uniqueMealsArray : Array<IMeal> = [] //Important: converts array to set, removing duplicates and then convert back to array
+
+  // const uniqueMealsArray = Array.from(new Set<IMeal>(orders)); //og Important: converts array to set, removing duplicates and then convert back to array
+  // var seenPks: Array<Number> = [];
+  // for (var currMeal in uniqueMealsArray) {
+  //   currMeal;
+  // }
+
   //bug: if cache is turned to true in store, in creates differing verisons of the same item with the same key so meals can show up multiple times in cart.
   if (uniqueMealsArray.length > 0) {
     return uniqueMealsArray.map((meal: IMeal) => {
-      var numInCart = orders.filter((curr: IMeal) => meal.pk === curr.pk).length;
+      var numInCart = orders.filter((curr: IMeal) => meal.pk === curr.pk)
+        .length;
       return (
         <section key={meal.pk}>
           <CartCard
