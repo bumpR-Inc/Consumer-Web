e8c5ae9d58762c2fa405f3e711c1f7c85c8306bb
Justin Quan <justquan@berkeley.edu>
2021-02-23 00:45:47 -0800

    added rounding for price breakdown

--- src/components/OrderingUI/CartPriceBreakdown.tsx
+++ src/components/OrderingUI/CartPriceBreakdown.tsx
@@ -20,29 +20,36 @@ interface CartProps {
 
 export default function CartPriceBreakdown({mealsCost, tax, tipAmt, deliveryFee, totalCost}: CartProps) {
   // const classes = useStyles();
+  var roundedMealsCost = Math.round(mealsCost * 100) / 100;
+  var roundedTax = Math.round(tax * 100) / 100;
+  var roundedDeliveryFee = Math.round(deliveryFee * 100) / 100;
+  var roundedTipAmt = Math.round(tipAmt * 100) / 100;
+  var roundedTotalCost = Math.round(totalCost * 100) / 100;
+
+
 
   return (
     <>
       <div className="cart-line"></div>
       <div className="cost-row">
         <p className="cart-text">Subtotal:</p>
-        <p className="cart-text">${mealsCost}</p>
+        <p className="cart-text">${roundedMealsCost}</p>
       </div>
       <div className="cost-row">
         <p className="cart-text">Tax:</p>
-        <p className="cart-text">${tax}</p>
+        <p className="cart-text">${roundedTax}</p>
       </div>
       <div className="cost-row">
         <p className="cart-text">Delivery Fee:</p>
-        <p className="cart-text">${deliveryFee}</p>
+        <p className="cart-text">${roundedDeliveryFee}</p>
       </div>
       <div className="cost-row">
         <p className="cart-text">Optional Tip:</p>
-        <p className="cart-text">${tipAmt}</p>
+        <p className="cart-text">${roundedTipAmt}</p>
       </div>
       <div className="cost-row">
         <p className="cart-text bolded">Total:</p>
-        <p className="cart-text bolded">${totalCost}</p>
+        <p className="cart-text bolded">${roundedTotalCost}</p>
       </div>
       <div className="cart-line"></div>
     </>
